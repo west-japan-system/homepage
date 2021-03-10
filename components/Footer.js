5c511c0409a8c4b92821f5f77ae5ff6a64a6f6e7
west-japan-system <79397824+west-japan-system@users.noreply.github.com>
2021-03-10 22:42:50 +0900

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>©️株式会社 West Japan System</small>
+    </footer>
+  );
+}
--- pages/company.js
+++ pages/company.js
@@ -1,127 +1,17 @@
-import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
-import React, { useState } from "react";
+import Meta from "../components/Meta";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function Company() {
-  const [open, setOpen] = useState(false);
-  const menuFunction = () => {
-    setOpen(!open);
-  };
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta http-equiv="content-language" content="ja" />
-        <meta
-          name="description"
-          content="株式会社 West Japan Systemの「会社概要」です。"
-        />
-        <meta name="theme-color" content="#000000" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="WestJapanSystem" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta
-          name="keywords"
-          content="WestJapanSystem,westjapansystem,ウエストジャパンシステム"
-        />
-        <meta property="og:title" content="株式会社 West Japan System" />
-        <meta property="og:type" content="article" />
-        <meta
-          property="og:description"
-          content="株式会社 West Japan Systemの「会社概要」です。"
-        />
-        <meta
-          property="og:url"
-          content="https://west-japan-system.com/company"
-        />
-        <meta
-          property="og:image"
-          content="https://west-japan-system.com/images/company_ogp-logo.png"
-        />
-        <meta property="og:site_name" content="株式会社 West Japan System" />
-        <meta property="og:locale" content="ja_JP" />
-        <noscript data-n-css="true"></noscript>
-        <link
-          rel="apple-touch-icon-precomposed"
-          href="/images/apple-touch-icon.png"
-        />
-        <link rel="canonical" href="https://west-japan-system.com" />
-        <link rel="icon" href="/favicon.ico" />
-        <title>会社概要 | 株式会社 West Japan System</title>
-      </Head>
-
-      <header className="header">
-        <nav className="header__menu">
-          <Link href="/">
-            <a className="header__menu-logo--position">
-              <Image
-                src="/images/company-logo.jpg"
-                alt="West Japan Systemロゴ"
-                width={195}
-                height={32}
-                className="header__menu-logo"
-              />
-            </a>
-          </Link>
-          <div className="header__navigation">
-            <Link href="/company">
-              <a className="header__menu-item">会社概要</a>
-            </Link>
-            <Link href="/contact">
-              <a className="header__menu-item">お問い合わせ</a>
-            </Link>
-            <button className="header__menu-icon">
-              <Image
-                src="/images/menu-icon.png"
-                alt="メニューバーのアイコン（ハンバーガー）"
-                width={40}
-                height={40}
-                onClick={() => menuFunction()}
-              />
-            </button>
-            <ul
-              className={`${"header__menu-mobile"} ${open ? "open" : "close"}`}
-            >
-              <div className="header__cross-icon--position">
-                <button className="header__cross-icon">
-                  <Image
-                    src="/images/cross-icon.png"
-                    alt="メニューバーのアイコン（クロス）"
-                    width={40}
-                    height={40}
-                    onClick={() => menuFunction()}
-                  />
-                </button>
-              </div>
-              <li className="header__menu-mobile--item">
-                <Link href="/">
-                  <a>Home</a>
-                </Link>
-              </li>
-              <li className="header__menu-mobile--item">
-                <Link href="/company">
-                  <a>会社概要</a>
-                </Link>
-              </li>
-              <li className="header__menu-mobile--item">
-                <Link href="/contact">
-                  <a>お問い合わせ</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div
-          className={`${"header__overlay"} ${open ? "open" : "hidden"}`}
-          onClick={() => menuFunction()}
-        ></div>
-      </header>
+      <Meta
+        title="会社概要 | 株式会社 West Japan System"
+        description="株式会社 West Japan Systemの「会社概要」です。"
+        ogType="article"
+        ogUrl="https://west-japan-system.com/company"
+      />
+      <Header />
       <main className="main">
         <section className="main__section-img">
           <h1 className="main__title">会社概要</h1>
@@ -169,9 +59,7 @@ export default function Company() {
           </dl>
         </section>
       </main>
-      <footer className="footer">
-        <small>©️株式会社 West Japan System</small>
-      </footer>
+      <Footer />
     </>
   );
 }
